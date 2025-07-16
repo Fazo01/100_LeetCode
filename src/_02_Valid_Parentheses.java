ee352aa40c04c219b239f74cd94c23d31e021f23
Fazo01 <fg7829098@gmail.com>
2025-07-16 22:40:01 +0500

    Complete _02_Valid_Parentheses

--- src/_02_Valid_Parentheses.java
+++ src/_02_Valid_Parentheses.java
@@ -1,3 +1,26 @@
-public class _02_Valid_Parentheses {
+import java.util.Stack;
 
+public class _02_Valid_Parentheses {
+    public static boolean isValid(String s){
+        Stack<Character>stack=new Stack<>();
+        for(char c:s.toCharArray()){
+            if (c=='('||c=='{'||c=='['){
+                stack.push(c);
+            }else {
+                if(stack.isEmpty()) return false;
+                char top=stack.pop();
+                if ((c==')'&& top!='(')||(c=='}'&& top!='{'||c==']'&& top!='[')){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        System.out.println(isValid("()"));      // true
+        System.out.println(isValid("()[]{}"));  // true
+        System.out.println(isValid("(]"));      // false
+        System.out.println(isValid("([])"));    // true
+
+    }
 }
