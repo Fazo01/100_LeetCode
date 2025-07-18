ec96a0b50f2b58c9a767137814c2049712fd4c5c
Fazo01 <fg7829098@gmail.com>
2025-07-18 20:07:58 +0500

    Initialize _04_Best_Time_To_Buy_And_Sell_Stock

---
+++ src/_04_Best_Time_To_Buy_And_Sell_Stock.java
@@ -0,0 +1,10 @@
+public class _04_Best_Time_To_Buy_And_Sell_Stock {
+    public int Maxprofit(int[] nums){
+
+    }
+    public static void main(String[] args) {
+        _04_Best_Time_To_Buy_And_Sell_Stock solution=new _04_Best_Time_To_Buy_And_Sell_Stock();
+        int[] nums={7,1,5,3,6,4};
+        System.out.println("Max profit: "+solution.Maxprofit(nums));
+    }
+}
