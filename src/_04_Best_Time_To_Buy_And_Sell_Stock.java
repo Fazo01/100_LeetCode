59a0b034e26ff981c18835fc07492db48af65e88
Fazo01 <fg7829098@gmail.com>
2025-07-18 22:49:30 +0500

    complete _04_Best_Time_To_Buy_And_Sell_Stock

--- src/_04_Best_Time_To_Buy_And_Sell_Stock.java
+++ src/_04_Best_Time_To_Buy_And_Sell_Stock.java
@@ -1,6 +1,15 @@
 public class _04_Best_Time_To_Buy_And_Sell_Stock {
     public int Maxprofit(int[] nums){
-
+        int minprice=Integer.MAX_VALUE;
+        int maxProfit=0;
+        for(int price:nums){
+            if (price<minprice){
+                minprice=price;
+            }else {
+                maxProfit=Math.max(maxProfit,price-minprice);
+            }
+        }
+        return maxProfit;
     }
     public static void main(String[] args) {
         _04_Best_Time_To_Buy_And_Sell_Stock solution=new _04_Best_Time_To_Buy_And_Sell_Stock();
