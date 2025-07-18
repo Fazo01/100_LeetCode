c82167b67041701408957d3b6395ad9ae0c38df8
Fazo01 <fg7829098@gmail.com>
2025-07-18 22:55:50 +0500

    Initialize _05_Valid_Palindrome

---
+++ src/_05_Valid_Palindrome.java
@@ -0,0 +1,13 @@
+public class _05_Valid_Palindrome {
+    public boolean isPalindrome(String s){
+        return ;
+    }
+    public static void main(String[] args) {
+        _05_Valid_Palindrome solution = new _05_Valid_Palindrome();
+
+        System.out.println(solution.isPalindrome("A man, a plan, a canal: Panama")); // true
+        System.out.println(solution.isPalindrome("race a car"));                     // false
+        System.out.println(solution.isPalindrome(" "));                              // true
+
+    }
+}
