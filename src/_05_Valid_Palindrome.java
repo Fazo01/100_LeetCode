4195ac745c4776628fc1209006c33886ca211e95
Fazo01 <fg7829098@gmail.com>
2025-07-19 18:57:51 +0500

    Complete _05_Valid_Palindrome

--- src/_05_Valid_Palindrome.java
+++ src/_05_Valid_Palindrome.java
@@ -1,6 +1,27 @@
 public class _05_Valid_Palindrome {
     public boolean isPalindrome(String s){
-        return ;
+        int left=0;
+        int right=s.length()-1;
+        while (left<right){
+            while (left<right&& !Character.isLetterOrDigit(s.charAt(left))){
+                //condition becomes right if the character is NOT a letter or digit from left.
+                left++;
+            }
+            while (left<right&& !Character.isLetterOrDigit(s.charAt(right))){
+                //condition becomes right if the character is NOT a letter or digit from left.
+                right--;
+            }
+
+            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
+                //run  if character from left and right side comes different
+                return false;
+            }
+
+            //move left and right position
+            left++;
+            right--;
+        }
+        return true;
     }
     public static void main(String[] args) {
         _05_Valid_Palindrome solution = new _05_Valid_Palindrome();
