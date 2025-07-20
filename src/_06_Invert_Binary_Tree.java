11614fb133993ce08f947311ba2a16fccfa59411
Fazo01 <fg7829098@gmail.com>
2025-07-20 22:15:21 +0500

    complete _06_Invert_Binary_Tree

--- src/_06_Invert_Binary_Tree.java
+++ src/_06_Invert_Binary_Tree.java
@@ -1,2 +1,41 @@
 public class _06_Invert_Binary_Tree {
+    /**
+     * Definition for a binary tree node.
+     */
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * Solution class with invertTree method.
+     */
+    public static class Solution {
+        public TreeNode invertTree(TreeNode root) {
+            if (root == null) return null;
+
+            TreeNode temp = root.left;
+            root.left = invertTree(root.right);
+            root.right = invertTree(temp);
+
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
