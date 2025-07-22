78ab2dd8998f148b01b8f8e990decc3c63b06f32
Fazo01 <fg7829098@gmail.com>
2025-07-22 12:13:42 +0500

    Finalize _08_Binary_Search

--- src/_08_Binary_Search.java
+++ src/_08_Binary_Search.java
@@ -2,7 +2,7 @@ public class _08_Binary_Search {
     public static int search(int[] nums,int target){
         int left=0;
         int right= nums.length-1;
-        while (left<right){
+        while (left<=right){
             int mid=(left+right)/2;
             if(nums[mid]==target)return mid;
             if (target>nums[mid]){
