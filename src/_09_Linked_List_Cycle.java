cda9c63bc7ab941ea1454248a61c6b6982387d5c
Fazo01 <fg7829098@gmail.com>
2025-07-23 17:57:48 +0500

    Initialize _09_Linked_List_Cycle

---
+++ src/_09_Linked_List_Cycle.java
@@ -0,0 +1,17 @@
+public class _09_Linked_List_Cycle {
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public class Solution {
+        public boolean hasCycle(ListNode head) {
+
+
+        }
+    }
+}
