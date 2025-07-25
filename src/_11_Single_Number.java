e8a35cce63a2b9aeab15ab4dc46346f2cfdfb4f6
Fazo01 <fg7829098@gmail.com>
2025-07-25 14:42:58 +0500

    Initialize _11_Single_Number

---
+++ src/_11_Single_Number.java
@@ -0,0 +1,17 @@
+public class _11_Single_Number {
+    public static int singleNumber(int[] nums) {
+
+    }
+
+    // Main method to test the solution
+    public static void main(String[] args) {
+        int[] nums1 = {2, 2, 1};
+        int[] nums2 = {4, 1, 2, 1, 2};
+        int[] nums3 = {1};
+
+        System.out.println("Single number in nums1: " + singleNumber(nums1)); // Output: 1
+        System.out.println("Single number in nums2: " + singleNumber(nums2)); // Output: 4
+        System.out.println("Single number in nums3: " + singleNumber(nums3)); // Output: 1
+    }
+
+}
