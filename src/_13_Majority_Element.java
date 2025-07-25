acf76d2c29a087f5dced3a62fca63d958048d42b
Fazo01 <fg7829098@gmail.com>
2025-07-25 22:09:50 +0500

    Initialize _13_Majority_Element

---
+++ src/_13_Majority_Element.java
@@ -0,0 +1,16 @@
+public class _13_Majority_Element {
+    public int majorityElement(int[] nums){
+
+    }
+
+    public static void main(String[] args) {
+        _13_Majority_Element solution = new _13_Majority_Element();
+
+        int[] nums1 = {3, 2, 3};
+        int[] nums2 = {2, 2, 1, 1, 1, 2, 2};
+
+        System.out.println("Majority Element in nums1: " + solution.majorityElement(nums1)); // Output: 3
+        System.out.println("Majority Element in nums2: " + solution.majorityElement(nums2)); // Output: 2
+    }
+
+}
