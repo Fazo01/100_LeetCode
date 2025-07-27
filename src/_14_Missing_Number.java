c1ee9a872a2cb81599cd848d1ac9472ff89f750d
Fazo01 <fg7829098@gmail.com>
2025-07-27 21:43:27 +0500

    Initialize _14_Missing_Number

---
+++ src/_14_Missing_Number.java
@@ -0,0 +1,30 @@
+public class _14_Missing_Number {
+    // Brute force solution
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<=n;i++){
+            boolean found=false;
+            for(int j=0;j<n;j++){
+                if (nums[j]==i){
+                    found=true;
+                    break;
+                }
+            }
+            if (!found){
+                return i;
+            }
+        }
+        return -1; // This shouldn't happen for valid input
+    }
+
+    public static void main(String[] args) {
+        _14_Missing_Number solution = new _14_Missing_Number();
+        int[] nums1 = {3, 0, 1};
+        int[] nums2 = {0, 1};
+        int[] nums3 = {9, 6, 4, 2, 3, 5, 7, 0, 1};
+
+        System.out.println("Output of nums1: " + solution.missingNumber(nums1)); // Expected: 2
+        System.out.println("Output of nums2: " + solution.missingNumber(nums2)); // Expected: 2
+        System.out.println("Output of nums3: " + solution.missingNumber(nums3)); // Expected: 8
+    }
+}
