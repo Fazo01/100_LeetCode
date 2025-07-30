92a9ba33d402b017f8eeb276e1d52adf755dafef
Fazo01 <fg7829098@gmail.com>
2025-07-30 23:18:14 +0500

    Initialize _16_Depth_of_Binary_Tree

---
+++ src/_16_Depth_of_Binary_Tree.java
@@ -0,0 +1,34 @@
+
+// Definition for a binary tree node.
+class TreeNode03 {
+    int val;
+    TreeNode03 left;
+    TreeNode03 right;
+
+    TreeNode03() {}
+    TreeNode03(int val) { this.val = val; }
+    TreeNode03(int val, TreeNode03 left, TreeNode03 right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class _16_Depth_of_Binary_Tree {
+
+    public int maxDepth(TreeNode03 root) {
+
+    }
+    public static void main(String[] args) {
+        // Constructing the binary tree: [3,9,20,null,null,15,7]
+        TreeNode03 root = new TreeNode03(3);
+        root.left = new TreeNode03(9);
+        root.right = new TreeNode03(20, new TreeNode03(15), new TreeNode03(7));
+
+        _16_Depth_of_Binary_Tree solution = new _16_Depth_of_Binary_Tree();
+        int result = solution.maxDepth(root);
+
+        System.out.println("Maximum depth: " + result);  // Output: 3
+    }
+
+}
