fedcb7f07c7695da0154a536802cc70648380810
Fazo01 <fg7829098@gmail.com>
2025-08-10 19:51:37 +0500

    Complete _17_Middle_of_the_Linked_List

--- src/_17_MIddle_of_the_Linked_List.java
+++ src/_17_MIddle_of_the_Linked_List.java
@@ -9,5 +9,13 @@ public class _17_MIddle_of_the_Linked_List {
     }
     public ListNode04 middleNode(ListNode04 head){
 
+        ListNode04 slow01 =head;
+        ListNode04 fast01=head;
+        while(fast01!=null&&fast01.next!=null){
+            slow01=slow01.next;
+            fast01=fast01.next.next;
+
+        }
+        return slow01;
     }
 }
