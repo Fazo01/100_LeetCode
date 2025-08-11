cf9e91f4052ac3d112256fd090952619b8198189
Fazo01 <fg7829098@gmail.com>
2025-08-11 21:35:43 +0500

    Initialize _18_Converted_Sorted_array_to_Binary_Search_Tree

---
+++ src/_18_Converted_Sorted_array_to_Binary_Search_Tree.java
@@ -0,0 +1,30 @@
+public class TreeNode05 {
+    int val;
+    TreeNode05 left;
+    TreeNode05 right;
+    TreeNode05() {}
+        TreeNode05(int val) {
+            this.val = val;
+        }
+        TreeNode05(int val, TreeNode05 left, TreeNode05 right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+}
+public class _18_Converted_Sorted_array_to_Binary_Search_Tree {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
