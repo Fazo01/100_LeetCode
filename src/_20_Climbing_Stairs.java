e2ca54788db2ce43bf18311bccc35667069d7229
Fazo01 <fg7829098@gmail.com>
2025-08-12 09:16:55 +0500

    complete _20_Climbing_Stairs

--- src/_20_Climbing_Stairs.java
+++ src/_20_Climbing_Stairs.java
@@ -1,6 +1,15 @@
 public class _20_Climbing_Stairs {
     public static int climbStairs(int n){
-
+        if(n<=2) return n;
+        int onestep=1;
+        int twostep=2;
+        int totalways=0;
+        for (int i=3;i<=n;i++){
+            totalways=onestep+twostep;//0=1+2
+            onestep=twostep;//1=2
+            twostep=totalways;//2=0 => 2
+        }
+        return totalways;
     }
     public static void main(String[] args) {
         for (int n = 1; n <= 6; n++) {
