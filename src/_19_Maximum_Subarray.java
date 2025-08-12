73193eccd8a5858c84bfb9e9d49b904063268f87
Fazo01 <fg7829098@gmail.com>
2025-08-12 08:59:08 +0500

    Complete _19_Maximum_Subarray

--- src/_19_Maximum_Subarray.java
+++ src/_19_Maximum_Subarray.java
@@ -1,6 +1,14 @@
 public class _19_Maximum_Subarray {
     public static int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
 
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(currentSum, maxSum);
+        }
+
+        return maxSum;
     }
 
     public static void main(String[] args) {
