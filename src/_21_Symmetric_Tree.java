d8def7cd6bab6393541f5076d05ead244590b82f
Fazo01 <fg7829098@gmail.com>
2025-08-13 13:09:42 +0500

    Complete _21_Symmetric Tree

--- src/_21_Symmetric_Tree.java
+++ src/_21_Symmetric_Tree.java
@@ -1,3 +1,10 @@
 public class _21_Symmetric_Tree {
-
+    public boolean isSymmetric(TreeNode05 root){
+        return root == null || isMirror(root.left,root.right);
+    }
+    public boolean isMirror(TreeNode05 left , TreeNode05 right){
+        if(left==null && right==null) return true;
+        if(left==null || right==null) return false;
+        return (left.val==right.val) && isMirror(left.left,right.right) && isMirror(left.right,right.left);
+    }
 }
